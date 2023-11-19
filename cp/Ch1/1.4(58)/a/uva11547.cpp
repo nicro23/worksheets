7acff2552b61c482fafb71f7ad17b4b50441699c
nicro23 <modyhamody2332@gmail.com>
2023-11-19 11:19:14 +0300

    cp a

---
+++ cp/Ch1/1.4(58)/a/uva11547.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        int x = ((n*567)/9 + 7492)*235 /47 -498;
+        int y = (x%100 - x%10) /10;
+        cout<<y;
+    }
+}
+
+    
