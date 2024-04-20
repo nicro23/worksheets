3dc18af64f524a725ae1590706c9aedaf165e85a
nicro23 <modyhamody2332@gmail.com>
2024-04-20 23:48:42 +0300

    edit 2

---
+++ cp/test.cpp
@@ -0,0 +1,13 @@
+for(int i =0;i<*2n;i++)
+
+    for(int j=0;j<n;j++)
+
+        if(j%2==0)
+            cout<<"##";
+        }
+        else
+            cout<<"..";
+        }
+    }
+    cout<<endl;
+}
--- proj/test.cpp
+++ proj/test.cpp
@@ -29,7 +29,7 @@ void map(char x[][166], int r, int c)
 	cout.flush();
 
 }
-void user_action(char x[][166],char dir, int& r, int& c)
+void user_action(char x[][166],char dir, int& r, int& c, int mat_r, int mat_c)
 {
 	if (dir == 'w')
 	{
@@ -53,15 +53,36 @@ void user_action(char x[][166],char dir, int& r, int& c)
 		{
 			x[r][c+i] = '>';
 			x[r][c + i - 1] = '-';
+			map(x,mat_r,mat_c);
 		}
 	}
 }
+void all(char x[][166],int mat_r,int mat_c,int usr_r,int usr_c,int enmy1_r,int enmy1_c)
+{
+	int i, j, k;
+	clear(x,mat_r,mat_c);
+	//prototype fixed objects
+	for (i = 0; i < mat_c; i++)
+	{
+		x[0][i] = '-';
+		x[44][i] = '-';
+	}
+	for (j = 1; j + 1 < mat_r; j++)
+	{
+		x[j][0] = '|';
+		x[j][165] = '|';
+	}
+	//prototype moving objects
+	x[usr_r][usr_c] = 1;
+	x[enmy1_r][enmy1_c] = 2;
+	map(x, mat_r, mat_c);
+}
 void main()
 {
-	int i, j, k, r, c, usr_r, usr_c, enmy1_r, enmy1_c;
+	int mat_r, mat_c, usr_r, usr_c, enmy1_r, enmy1_c;
 	char x[45][166];
-	r = 45;
-	c = 166;
+	mat_r = 45;
+	mat_c = 166;
 	usr_r = 22;
 	usr_c = 42;
 	enmy1_r = 22;
@@ -70,24 +91,9 @@ void main()
 	{
 		while (!_kbhit())
 		{
-			clear(x, r, c);
-			//prototype fixed objects
-			for (i = 0; i < c; i++)
-			{
-				x[0][i] = '-';
-				x[44][i] = '-';
-			}
-			for (j = 1; j + 1 < r; j++)
-			{
-				x[j][0] = '|';
-				x[j][165] = '|';
-			}
-			//prototype moving objects
-			x[usr_r][usr_c] = 1;
-			x[enmy1_r][enmy1_c] = 2;
-			map(x, r, c);
+			all(x,mat_r,mat_c,usr_r,usr_c,enmy1_r,enmy1_c);
 		}
 		char move = _getch();
-		user_action(x, _getch(), usr_r, usr_c);
+		user_action(x, _getch(), usr_r, usr_c, mat_r, mat_c);
 	}
 }
