fe95214048a037da071147944c6df91b167eab7d
nicro23 <modyhamody2332@gmail.com>
2024-11-19 08:23:24 +0200

    l

--- co/lll2.asm
+++ co/lll2.asm
@@ -3,6 +3,6 @@
 main:
 	li	$t1, 0x24681357
 	andi 	$t2, $t1, 0xff
-	ori	$t3, $t1, 0xff000000
+	ori	$t3, $t1, 0x00ff0000
 	li $v0, 10
 	syscall
--- co/lll3.asm
+++ co/lll3.asm
@@ -1,9 +1,11 @@
 	.text
 	.globl main
 main:
-	andi
-	ori
-	srl
-	sll
-	or
+	li $t0, 0x12345678
+	andi $t1, $t0, 0xffffff00
+	ori $t2, $t1, 0xab
+	srl $t3, $t0, 16
+	sll $t4, $t0, 16
+	or  $t5, $t3, $t4
+	syscall
 	
