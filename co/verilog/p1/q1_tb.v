fefbf4b5f57d359afbd45af70f39dfa57acde7cb
zanaty <33067800+nicro23@users.noreply.github.com>
2024-12-03 09:27:54 +0300

    notihgn

--- co/verilog/p1/q1_tb.v
+++ co/verilog/p1/q1_tb.v
@@ -1,13 +1,13 @@
 `include "q1.v"
 
-module q1_tb;
+module q1_b;
 reg a,b,c,d,e;
 wire y;
 ComCir1 uut(.a(a),.b(b),.c(c),.d(d),.e(e),.y(y));
 
 initial begin
 $dumpfile = ("q1_tb.vcd");
-$dumpvars = (0, q1_tb);
+$dumpvars = (0, q1_b);
 $monitor = ("time = %0t, e = %b, y = %b", $time, e, y);
 
 for(integer i = 0; i < 32; i++)
